42bf167242a060ce9a206af08794daf2dbc57dde
wei.mao <wei.mao@jimubox.com>
2016-11-01 13:43:00 +0800

    core data

---
+++ Smashtag/Smashtag/TweetersTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TweetersTableViewController.swift
+//  Smashtag
+//
+//  Created by Mao on 31/10/2016.
+//  Copyright © 2016 cdts. All rights reserved.
+//
+
+import UIKit
+
+class TweetersTableViewController: UITableViewController {
+}
---
+++ Smashtag/Smashtag/TwitterUser.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterUser.swift
+//  Smashtag
+//
+//  Created by Mao on 31/10/2016.
+//  Copyright © 2016 cdts. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class TwitterUser: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
